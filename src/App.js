056bbd7b7bd3647688caadb04231a187ba645033
mine1279 <khun_neramid@hotmail.co.th>
2024-01-15 11:34:24 +0700

    EvenData2 in function component
    EvenData2 in function component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import React from 'react'
 import Button from './class-components';
 import { Calculator2 } from './calculator';
+import { EventData2 } from './event-data';
 
 
 export default function App(){
-    return <Calculator2/>
+    return <EventData2/>
 }
