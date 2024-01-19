83dff674d0cecbf76c01079451c34636aede5379
mine1279 <khun_neramid@hotmail.co.th>
2024-01-19 14:17:58 +0700

    refs func
    refs func

--- src/App.js
+++ src/App.js
@@ -2,8 +2,9 @@ import React from 'react'
 import Button from './class-components';
 import { Calculator2 } from './calculator';
 import { EventData2 } from './event-data';
+import RefsFunc from './refs-func';
 
 
 export default function App(){
-    return <EventData2/>
+    return <RefsFunc/>
 }
