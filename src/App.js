4b736b633a42bc888ba3ae7a1ba24aafc279f422
mine1279 <khun_neramid@hotmail.co.th>
2024-01-19 15:11:45 +0700

    refs array.js
    refs array.js

--- src/App.js
+++ src/App.js
@@ -3,8 +3,9 @@ import Button from './class-components';
 import { Calculator2 } from './calculator';
 import { EventData2 } from './event-data';
 import RefsFunc from './refs-func';
+import RefsArray from './refs-array';
 
 
 export default function App(){
-    return <RefsFunc/>
+    return <RefsArray/>
 }
