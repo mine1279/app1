86aab4a71e651620e8c5d4e89aa7dd17335c6504
mine1279 <khun_neramid@hotmail.co.th>
2024-01-19 15:48:42 +0700

    state func
    state func

--- src/App.js
+++ src/App.js
@@ -4,8 +4,9 @@ import { Calculator2 } from './calculator';
 import { EventData2 } from './event-data';
 import RefsFunc from './refs-func';
 import RefsArray from './refs-array';
+import MessageBox from './state-func';
 
 
 export default function App(){
-    return <RefsArray/>
+    return <MessageBox/>
 }
