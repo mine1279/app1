6d4fabb6e9a2e4e1b19d0e015e96a2e7d4e056ea
mine1279 <khun_neramid@hotmail.co.th>
2024-02-19 09:45:27 +0700

    form-search.js
    form-search.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React from "react";
 import Router2 from "./router2";
 import Router3 from "./router3";
+import FormSearch from "./form-search";
 
 export default function App(){
-    return <Router3/>
+    return <FormSearch/>
 }
