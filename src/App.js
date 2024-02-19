b68aef5b17fccb8cfa4bd941aba8a600edcd36f2
mine1279 <khun_neramid@hotmail.co.th>
2024-02-19 11:07:56 +0700

    react-bootstrap
    react-bootstrap

--- src/App.js
+++ src/App.js
@@ -2,7 +2,9 @@ import React from "react";
 import Router2 from "./router2";
 import Router3 from "./router3";
 import FormSearch from "./form-search";
+import FormBootstrap from "./form-bootstrap";
+import FormText from "./form-text";
 
 export default function App(){
-    return <FormSearch/>
+    return <FormText/>
 }
