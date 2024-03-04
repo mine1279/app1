c78406fe17d6f47bd57f86fc1473480e00889217
mine1279 <khun_neramid@hotmail.co.th>
2024-03-04 12:17:17 +0700

    form-validation
    form-validation

--- src/App.js
+++ src/App.js
@@ -5,7 +5,8 @@ import FormSearch from "./form-search";
 import FormBootstrap from "./form-bootstrap";
 import FormText from "./form-text";
 import FormCheck from "./form-check";
+import FormValidation from "./form-validation";
 
 export default function App(){
-    return <FormCheck/>
+    return <FormValidation/>
 }
