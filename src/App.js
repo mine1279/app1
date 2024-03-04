74c21696e79a04a35cc2a3d5d6c437c348a016e9
mine1279 <khun_neramid@hotmail.co.th>
2024-03-04 10:41:32 +0700

    form-check
    form-check

--- src/App.js
+++ src/App.js
@@ -4,7 +4,8 @@ import Router3 from "./router3";
 import FormSearch from "./form-search";
 import FormBootstrap from "./form-bootstrap";
 import FormText from "./form-text";
+import FormCheck from "./form-check";
 
 export default function App(){
-    return <FormText/>
+    return <FormCheck/>
 }
